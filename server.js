2081e7c99d53e3e706ccccabd40174ae5699d186
Svyatik4130 <barberbi1@icloud.com>
2021-07-09 15:41:45 +0300

    setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const path = require('path');
+
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 4000
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err) => {
+    if (err) throw err; 
+    console.log("MONGODB CONNECTED")
+})
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, '/frontend/build/index.html')) // important for 404!!!
+    })
+}
+
+app.listen(PORT, () => console.log(`the server started at ${PORT}`))
