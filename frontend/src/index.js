683313946b81b6406bac2b84609bf85ebc1dbc59
Svyatik4130 <barberbi1@icloud.com>
2021-07-11 14:07:02 +0300

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom';
 import { hydrate, render } from "react-dom";
 
-const APP = (
-    <App />
-)
+// const APP = (
+//     <App />
+// )
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(APP, rootElement);
+// } else {
+//   render(APP, rootElement);
+// }
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
