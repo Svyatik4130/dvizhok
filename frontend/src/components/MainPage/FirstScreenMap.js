55db2ebba4358f8b31ab40ec3b19768ed658c7a7
Svyatik4130 <barberbi1@icloud.com>
2021-07-18 17:14:14 +0300

    bg video for mob devices

--- frontend/src/components/MainPage/FirstScreenMap.js
+++ frontend/src/components/MainPage/FirstScreenMap.js
@@ -34,7 +34,8 @@ export default function FirstScreenMap() {
                     </div>
                 </div>
                 <div className="bg-cover">
-                    <video id="video" onContextMenu={(e) => e.preventDefault()} src="images/1screen_short_cropped.mp4" preload="auto" playsinline={true} autoPlay={true} loop={true} muted="muted" className="-z-10 w-full h-screen object-cover"></video>
+                    <video id="video" onContextMenu={(e) => e.preventDefault()} src="images/1screen_short_cropped.mp4" preload="auto" playsinline={true} autoPlay={true} loop={true} muted="muted" className="-z-10 w-full h-screen object-cover hidden md:block"></video>
+                    <img src="images/landing/bgvideo.gif" alt="dvizhok" className="-z-10 w-full h-screen object-cover md:hidden" />
                 </div>
                 <div id="idea" className='w-full mb-16 h-0.5'></div>
             </div>
