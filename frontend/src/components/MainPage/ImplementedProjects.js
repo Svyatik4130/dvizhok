85a5c6fec1809faa7230da405f925387f56a5f64
Svyatik4130 <barberbi1@icloud.com>
2021-07-20 11:34:16 +0300

    implemented project for larger displays

--- frontend/src/components/MainPage/ImplementedProjects.js
+++ frontend/src/components/MainPage/ImplementedProjects.js
@@ -2,24 +2,41 @@ import React, { useState } from 'react'
 
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import descriptions from './sources/descs.json'
 
 export default function ImplementedProjects() {
     const [SelectedProj, setSelectedProj] = useState({
         title: "СТАРОНАВОДНИЦЬКА БАШТА",
         desc: "Ми гуртуємо людей, які не чекають дива, а самі творять дива. Творцям Омріяної Країни ми даємо мережеві можливості для спілкування, обміну досвідом, взаємної пдтримки та cпівпраці.",
-        video_src: ""
+        video_src: "images/1screen_short_cropped.mp4"
     })
     const [lastSelected, setlastSelected] = useState()
 
     const selectProj = (e) => {
-        if(lastSelected){
+        if (lastSelected) {
             console.log(lastSelected)
             lastSelected.style.top = "0px"
         }
         let target = e.target.parentElement
         console.log(target)
+
+        let title = target.querySelector('.project_title').innerHTML.toLowerCase()
+        console.log(title)
         target.style.top = "-15px"
         setlastSelected(target)
+
+        let findArr = descriptions.find(e => e.title.toLowerCase() === title)
+        console.log(findArr)
+        if (findArr) {
+            console.log("good")
+            setSelectedProj({
+                title: findArr.title,
+                desc: findArr.desc,
+                video_src: findArr.videosrc
+            })
+        }
+
+        console.log(SelectedProj.title)
     }
 
     return (
@@ -34,67 +51,83 @@ export default function ImplementedProjects() {
                     </div>
                 </div>
                 <div className=" w-full order-1 lg:order-2 lg:w-7/12">
-                    <video src="images/1screen_short_cropped.mp4" preload="auto" controls={true} className=" w-full rounded-xl"></video>
+                    <video src={SelectedProj.video_src} preload="auto" controls={true} className=" w-full rounded-xl"></video>
                 </div>
             </div>
 
             <Carousel autoPlay={false} showThumbs={false} showStatus={false} className=" hidden lg:block prpl-btns mt-24">
                 <div className="w-full pt-5 flex px-6 pb-12">
-                    <div onClick={selectProj} className="w-3/12 cursor-pointer px-4">
-                        <div className="w-full top-0 transition-all relative bg-cover">
+                    <div className="w-3/12 px-4">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
                             <img src="images/landing/implemented_projects/impl_proj_1.png" alt="impl_proj_1" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
-                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Майстерня переробки пластику EcoREactive</h1>
+                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Майстерня переробки пластику EcoREactive</p>
                         </div>
                     </div>
-                    <div onClick={selectProj} className="w-3/12 cursor-pointer px-4">
-                        <div className="w-full top-0 transition-all relative bg-cover">
+                    <div className="w-3/12 px-4">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
                             <img src="images/landing/implemented_projects/impl_proj_2.png" alt="impl_proj_2" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Старонаводницька Башта</h1>
                             <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
                         </div>
                     </div>
                     <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
                             <img src="images/landing/implemented_projects/impl_proj_3.png" alt="impl_proj_3" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
-                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">EdCamp діє!</h1>
+                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">EdCamp діє!</p>
                         </div>
                     </div>
                     <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
                             <img src="images/landing/implemented_projects/impl_proj_4.png" alt="impl_proj_4" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Старонаводницька Башта</h1>
                             <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
                         </div>
                     </div>
                 </div>
-                <div className="w-full flex px-6 pb-12">
+                <div className="w-full pt-5 flex px-6 pb-12">
                     <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
                             <img src="images/landing/implemented_projects/impl_proj_1.png" alt="impl_proj_1" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Майстерня переробки пластику EcoREactive</h1>
+                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Майстерня переробки пластику EcoREactive</p>
+                        </div>
+                    </div>
+                    <div className="w-3/12 px-4">
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
+                            <img src="images/landing/implemented_projects/impl_proj_2.png" alt="impl_proj_2" className="rounded-lg w-full" />
+                            <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Старонаводницька Башта</h1>
                             <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
                         </div>
                     </div>
                     <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
-                            <img src="images/landing/implemented_projects/impl_proj_1.png" alt="impl_proj_1" className="rounded-lg w-full" />
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
+                            <img src="images/landing/implemented_projects/impl_proj_3.png" alt="impl_proj_3" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
-                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">EdCamp діє!</h1>
+                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">EdCamp діє!</p>
                         </div>
                     </div>
                     <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
-                            <img src="images/landing/implemented_projects/impl_proj_1.png" alt="impl_proj_1" className="rounded-lg w-full" />
+                        <div onClick={selectProj} className="cursor-pointer w-full top-0 transition-all relative bg-cover">
+                            <img src="images/landing/implemented_projects/impl_proj_4.png" alt="impl_proj_4" className="rounded-lg w-full" />
                             <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
-                            <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
-                        </div>
-                    </div>
-                    <div className="w-3/12 px-4">
-                        <div className="w-full relative bg-cover">
-                            <img src="images/landing/implemented_projects/impl_proj_1.png" alt="impl_proj_1" className="rounded-lg w-full" />
-                            <div className="w-full h-full absolute top-0 rounded-lg darker-image z-30"></div>
+                            {/* Enter real title of this project */}
+                            <h1 className="hidden project_title">Старонаводницька Башта</h1>
                             <p className="absolute font-bold text-lg bottom-3 text-white left-0 right-0 z-40 ml-auto mr-auto text-center">Старонаводницька Башта</p>
                         </div>
                     </div>
