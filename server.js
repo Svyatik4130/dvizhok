026a6bdc3ac386c49bf443fd71ff13e2e339742f
Svyatik <barberbi1@icloud.com>
2021-09-15 09:50:30 +0200

    try 1

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cors = require('cors')
 app.use(cors())
 
 app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 const PORT = process.env.PORT || 5040
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
