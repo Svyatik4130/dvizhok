9ea0b4974bfdcc64c37ddfce68d2044496654bb2
Svyatik <barberbi1@icloud.com>
2021-10-25 10:38:29 +0200

    messenger 1.1 for mob devices

--- frontend/src/components/Dashboard/Pages/Messenger/Panel.js
+++ frontend/src/components/Dashboard/Pages/Messenger/Panel.js
@@ -158,7 +158,7 @@ export default function Panel() {
                             <div className="w-full flex pb-8 lg:hidden">
                                 <svg onClick={() => { setisOnlineUsersListExpandedMob(true); setmobileStylesForOnlineUsers("absolute right-0") }} className='absolute right-0' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#48004B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
                             </div>
-                            <div className="overflow-y-scroll h-full" ref={scrollRef}>
+                            <div className="overflow-y-scroll pb-20 lg:pb-0 h-full" ref={scrollRef}>
                                 {messages.map((m) => (
                                     <div ref={scrollRef} key={m._id}>
                                         <Message message={m} own={m.sender === user.id} friend={friend} />
